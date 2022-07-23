b2fa099b0fc7003eeb91c059106215243ebf1ac8
Malaclypse <jedijor14@gmail.com>
2022-07-23 13:43:15 -0500

    added edit & update with error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,19 @@
 class ArticlesController < ApplicationController
     def show
-        #debugger    
+        
         @article = Article.find(params[:id])
     end
-    def index
-        #debugger    
+    
+    def index     
         @articles = Article.all
     end
 
     def new
         @article = Article.new()
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -18,7 +22,19 @@ class ArticlesController < ApplicationController
             flash[:notice] = "Article was created successfully."
             redirect_to @article
         else 
-            render "new"
+            render :new, status: :unprocessable_entity
         end
     end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else 
+            render :edit, status: :unprocessable_entity
+        end
+
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home' #for a homepage
 	get 'about', to: 'pages#about' #for and about page
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
