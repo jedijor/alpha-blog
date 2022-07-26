8c62639f1aa7cb07e2a0f1abec6df4bb3479d736
Malaclypse <jedijor14@gmail.com>
2022-07-25 22:54:07 -0500

    added navigation link and fixed the DELETE option in every page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,15 @@
 class ArticlesController < ApplicationController
-    def show
-        
-        @article = Article.find(params[:id])
-    end
     
     def index     
         @articles = Article.all
+    end
+
+    def show
+        if Article.find(params[:id])
+            @article = Article.find(params[:id])
+        else
+            redirect_to articles_path
+        end
     end
 
     def new
@@ -34,6 +38,13 @@ class ArticlesController < ApplicationController
         else 
             render :edit, status: :unprocessable_entity
         end
+
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path, status: :see_other
 
     end
 
